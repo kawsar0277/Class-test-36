8c594d41b20f454f5cfa3a8447b128d1fad10b2b
kawsar0277 <kawsar1252005@gmail.com>
2025-07-02 16:38:24 +0600

    added class 36 in this repository

---
+++ script.js
@@ -0,0 +1,66 @@
+let number1;
+let number2;
+
+
+number1 = 541;
+
+number2 = 200;
+
+if(number1 > number2){
+    console.log("number1 is maximum")
+}
+
+else{
+    console.log("number2 is maximum")
+}
+
+
+//problem
+
+
+let number;
+
+number = 215;
+
+if(number > 0){
+    console.log("the number is positive")
+}
+
+else if(number < 0){
+    console.log("the number is negetive")
+}
+
+else{
+    console.log("the number is 0")
+}
+
+
+//problem
+
+let digit;
+
+digit = 853;
+
+if(digit % 2 == 0){
+    console.log("the number is even")
+}
+
+else{
+    console.log("the number is odde")
+    
+}
+
+
+//hw
+
+let digit2;
+
+digit2 = 5455452585
+
+if(digit2 % 11 == 0){
+    console.log("the number is even")
+}
+
+else{
+    console.log("the number is odd")
+}
